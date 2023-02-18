b738f73ff89d66f0bb000eef398b59b7fd28ed20
GuilhermeBarboosa <guilherme.strg@gmail.com>
2023-02-18 20:40:14 -0200

    feat: treino de RNA

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-# aplicativo para verificar se o ser vivo eh quadrupede ou bipede
-# quadrupede = 1, bipede = -1
-# cao = [-1,-1,1,1] | resposta = 1
-# gato = [1,1,1,1] | resposta = 1
-# cavalo = [1,1,-1,1] | resposta = 1
-# homem = [-1,-1,-1,1] | resposta = -1
 
-# pesos (sinapses)
 w = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# entradas
+
 x = [
 [-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,1,1,1],
 [-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1],
@@ -35,23 +26,28 @@ x = [
 [1,1,1,-1,-1,1,1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,-1,1,1],
 ]
 
-# respostas esperadas
 t = [1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1]
-# bias (ajuste fino)
+
+
+vetor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+testes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 b = 0
-# saida
+
 y = 0
-# numero maximo de interacoes
+
 max_int = 1000
-# taxa de aprendizado
+
 taxa_aprendizado = 0.0025
-# soma
+
 soma = 0
 
-# nome do animal
 letra = ""
-# resposta = acerto ou falha
+
 resposta = ""
+
+letraDesejada = "";
 
 # dicionario de dados
 d = {'-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,1,1,1':'a',
@@ -79,44 +75,37 @@ d = {'-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1
 print("Treinando")
 
 
-# funcao para converter listas em strings
 def listToString(list):
     s = str(list).strip('[]')
     s = s.replace(' ', '')
     return s
 
 
-# inicio do algoritmo
 for k in range(1, max_int):
     acertos = 0
     print("INTERACAO " + str(k) + "-------------------------")
     for i in range(0, len(x)):
         soma = 0
 
-        # pega o nome do animal no dicionário
         if d.get(listToString(x[i])):
             letra = d[listToString(x[i])]
         else:
             letra = ""
 
-        # para calcular a saida do adaline, cada entrada de x eh multiplicada
-        # pelo seu peso w correspondente
         for j in range(0, len(x[i])):
+
             soma += x[i][j] * w[j]
 
-        # a saida eh igual a soma anterior
-        y_in = soma
-        # print("y_in = ",str(y_in))
+            y_in = soma
+            testes[i] = soma;
 
-        # funcao de ativação
         if y_in >= 0:
             y = 1
+
         else:
             y = -1
 
-        # atualiza os pesos
         for j in range(0, len(w)):
-            # regra delta
 
             w[j] = w[j] + taxa_aprendizado * (t[i] - y_in) * x[i][j]
 
@@ -126,17 +115,51 @@ for k in range(1, max_int):
         else:
             resposta = "erro"
 
-        # imprime a resposta
         if y == 1:
-            print("Resposta: " + resposta + "\nLetra correta: " + letra)
-        elif y == 0:
-            print("Padrao nao identificado")
+            print("Resposta: " + resposta + "\nLetra: " + letra)
         elif y == -1:
-            print("Resposta: " + resposta + "\nLetra errada: " + letra)
+            print("Resposta: " + resposta + "\nLetra: " + letra)
 
     if acertos == len(x):
         print("Funcionalidade aprendida com " + str(k) + " interacoes")
-        print("A LETRA ESCOLHIDA FOI" + letra)
+
+
+        # DAQUI PRA BAIXO É PARA TESTAR COM LETRAS
+
+        # print(t)
+        # print("b" + str(b))
+        # print("y" + str(y))
+        # print("soma" + str(soma))
+        # print("taxa_aprendizado" + str(taxa_aprendizado))
+        # print("testes", str(testes))
+
+        # vetorSaida = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # teste = x[4];
+        #
+        # # print(testes);
+        # for i in range(0, len(x)):
+        #     soma = 0
+        #     for j in range(0, len(x[i])):
+        #         soma = soma + teste[j]*w[i]
+        #         testes[i] = soma + w[i]
+
+
+        # print(testes)
+
+        # for j in range(0, len(x)):
+        #     if testes[j] >= y:
+        #         vetorSaida[j] = 1
+        #     else:
+        #         vetorSaida[j] = -1
+
+        # print(vetorSaida)
+
+        # print(vetor)
+
         break
+
+
+
+
     print("")
 print("Finished")
